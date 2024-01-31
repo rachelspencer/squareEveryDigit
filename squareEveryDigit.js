6f2891951294f842784e9dfef2d92081484c887f
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-31 14:00:03 -0500

    solution

--- squareEveryDigit.js
+++ squareEveryDigit.js
@@ -2,13 +2,23 @@
 // and concatenate them.
 
 // For example, if we run 9119 through the function, 811181 will come out, 
-// because 92 is 81 and 12 is 1. (81-1-1-81)
+// because 9 squared is 81 and 1 squared  is 1. (81-1-1-81)
 
-// Example #2: An input of 765 will/should return 493625 because 72 is 49, 
-// 62 is 36, and 52 is 25. (49-36-25)
+// Example #2: An input of 765 will/should return 493625 because 7 squared  is 49, 
+// 6 squared  is 36 , and 5 squared  is 25. (49-36-25)
 
 // Note: The function accepts an integer and returns an integer.
 
 function squareDigits(num){
-    return 0;
-  };
+    const numArr = String(num).split("");
+  
+      const result = numArr.map((num) => {
+          return num ** 2
+      } 
+      )
+       
+      return parseInt(result.join(""))
+  }
+
+
+console.log(squareDigits(9119));
